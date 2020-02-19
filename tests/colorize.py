7f200e75134ddd2f3a4c1eaf0678cdb47448835b
Zephyr Pellerin <zephyr.pellerin@gmail.com>
2020-02-19 12:18:34 -0800

    add '-r' argument `tests/colorize.py` to print each engine's regex

--- tests/colorize.py
+++ tests/colorize.py
@@ -2,6 +2,7 @@ import csv
 import os
 
 import polyunite
+import sys
 
 
 def seen(f):
@@ -9,9 +10,17 @@ def seen(f):
         yield from csv.reader(csvfile)
 
 
-for name, family in seen(f'{os.path.dirname(__file__)}/fixtures/engine_families.csv'):
-    if name in polyunite.Schemes:
-        res = polyunite.Schemes[name](family)
-        print('%-10s' % name, res.colorize())
-        # print("ENGINE: %s = %s" % (engine, NamingScheme._engines[NamingScheme.to_engine(engine)].rgx))
-        # print(res)
+if len(sys.argv) == 1:
+    for name, family in seen(f'{os.path.dirname(__file__)}/fixtures/engine_families.csv'):
+        if name in polyunite.Schemes:
+            print('%-10s' % name, polyunite.Schemes[name](family).colorize())
+    sys.exit(0)
+
+elif len(sys.argv) == 2:
+    if sys.argv[1] == '-r':
+        # print regular expressions for each engine
+        for name, engine in polyunite.Schemes.items():
+            print("%s: \n\n%s\n" % (name, engine.rgx))
+        sys.exit(0)
+
+raise NotImplementedError
