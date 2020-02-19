41b54e47ce0d43817aeffd6ada21e47bb3fe5408
Zephyr Pellerin <zephyr.pellerin@gmail.com>
2020-02-19 02:14:40 -0800

    add colorization

--- setup.py
+++ setup.py
@@ -8,9 +8,6 @@ from setuptools import setup, find_packages
 with open('README.rst') as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
-
 requirements = [ ]
 
 setup_requirements = ['pytest-runner', ]
@@ -31,9 +28,12 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
     ],
+    entry_points={
+        'console_scripts': [ 'main=polyunite.polyunite:main' ]
+    },
     description="polyunite parses a variety of antimalware vendor's classification strings",
     install_requires=requirements,
-    long_description=readme + '\n\n' + history,
+    long_description=readme,
     include_package_data=True,
     keywords='polyunite',
     name='polyunite',
--- tests/test_polyunite.py
+++ tests/test_polyunite.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 """Tests for `polyunite` package."""
 
 import pytest
-
 
 from polyunite import polyunite
 
