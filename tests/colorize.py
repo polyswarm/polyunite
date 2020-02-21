4a23cfe2446f595529c8bfe5a627281ddb2b9d86
Zephyr Pellerin <zephyr.pellerin@gmail.com>
2020-02-20 19:09:11 -0800

    Read a zipped & deduplicated copy of `engine_families.csv`

--- tests/colorize.py
+++ tests/colorize.py
@@ -1,41 +1,49 @@
 import csv
 import os
 import sys
+import zipfile
+
+import pkg_resources
 
 import polyunite
 
 
-def seen(f):
-    with open(f, newline='') as csvfile:
-        yield from csv.reader(csvfile)
+def seen():
+    with zipfile.ZipFile(pkg_resources.resource_filename(__name__, f'fixtures/engine_families.zip')) as zf:
+        with zf.open('engine_families.csv') as csvfile:
+            yield from csv.reader(map(bytes.decode, csvfile.readlines()))
 
 
 if len(sys.argv) == 1:
-    for name, family in seen(f'{os.path.dirname(__file__)}/fixtures/engine_families.csv'):
-        if name in polyunite.Schemes:
-            sch = polyunite.Schemes[name](family)
+    missing = set()
+    for engine, family in seen():
+        sch = polyunite.Schemes.parse(engine, family)
+        if sch:
             try:
                 print(
-                    '{:<10} {:1} {:1} {:>9.9} {:>10.10} {:>12.12} {:85}'.format(
-                        name,
+                    '{:<10} {:1} {:1} {:<15.12} {:30.30} {:>16.16} {}'.format(
+                        engine,
                         'H' if sch.heuristic else ' ',
                         'T' if sch.malice_unlikely else ' ',
                         str(sch.operating_system or '    '),
-                        str(sch.label),
+                        str(', '.join(sch.label)),
                         str(sch.name),
                         sch.colorize(),
                     )
                 )
             except TypeError as e:
-                print("Couldn't parse '%s' as a %s name" % (family, name))
+                print("Couldn't parse '%s' as a %s name" % (family, engine))
                 print("Err: ", e)
+        else:
+            missing.add(engine)
+    print("No name scheme found for: ", missing)
     sys.exit(0)
 
 elif len(sys.argv) == 2:
     if sys.argv[1] == '-r':
         # print regular expressions for each engine
-        for name, engine in polyunite.Schemes.items():
-            print("%s: \n%s\n" % (name, engine.rgx.pattern))
+        for engine, obj in polyunite.Schemes.items():
+            print("%s: \n%s\n" % (engine, obj.rgx.pattern))
         sys.exit(0)
 
 raise NotImplementedError
