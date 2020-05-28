fe3b69c7cfdbbfde0c7041d018c7c0f183c3b9cd
Zephyr Pellerin <zephyr.pellerin@gmail.com>
2020-05-28 09:22:50 -0700

    Improve matches

--- polyunite/utils.py
+++ polyunite/utils.py
@@ -1,11 +1,29 @@
+from typing import Optional
+
+
 class colors:
     BLACK_FG = '\033[30m'
-    BLUE_FG = '\033[34m'
-    CYAN_FG = '\033[36m'
-    GREEN_FG = '\033[32m'
-    MAGENTA_FG = '\033[35m'
     RED_FG = '\033[31m'
-    WHITE_FG = '\033[37m'
+    GREEN_FG = '\033[32m'
     YELLOW_FG = '\033[33m'
+    BLUE_FG = '\033[34m'
+    MAGENTA_FG = '\033[35m'
+    CYAN_FG = '\033[36m'
+    WHITE_FG = '\033[37m'
+    ORANGE_FG = '\033[91m'
+    PINK_FG = '\033[95m'
     RESET = '\033[0m'
     UNDERLINE = '\033[4m'
+
+
+def group(*choices, fmt='(?:{})', name: 'Optional[str]' = None):
+    """Group a regular expression"""
+    spec = '(?P<%s>{})' % name if name else fmt
+    return spec.format('|'.join(set(map(format, filter(None, choices)))))
+
+class format_template(str):
+    __format__ = str.format
+
+# consume `{0}` if matches, but ensures but ensures this match is preceeded by `{0}`, this is
+# useful for regular expressions where the earlier match may have already consumed {0}.
+antecedent = format_template(r'{0}?(?<={0})')
