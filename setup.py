2849848f10a1501c1e20a58d8fbd0f16fb791380
Zephyr Pellerin <zephyr.pellerin@gmail.com>
2020-08-05 13:08:27 -0700

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -29,6 +29,6 @@ setup(
     test_suite='tests',
     tests_require=['pytest~=6.0.2', ],
     url='https://github.com/polyscore/polyunite',
-    version='0.1.0',
+    version='0.1.1',
     zip_safe=True,
 )
