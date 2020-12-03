0faad519404eb37d24adf8f98933e32fba01aee6
Zephyr Pellerin <zephyr.pellerin@gmail.com>
2020-12-02 21:55:07 -0800

    Remove `__all__`

--- polyunite/__init__.py
+++ polyunite/__init__.py
@@ -7,5 +7,3 @@ decode = registry.decode
 is_heuristic = registry.is_heuristic
 summarize = registry.summarize
 infer_name = registry.infer_name
-
-__all__ = ['parsers', 'vocab', 'errors', 'registry', 'parse', 'decode', 'is_heuristic', 'guess_name']
