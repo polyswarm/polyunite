6137a436d27e4f78fb0a47868c21d802fa75cf0b
Zephyr Pellerin <zephyr.pellerin@gmail.com>
2020-12-15 15:27:02 -0800

    Fix `is_heuristic`

---
+++ tests/test_polyunite.py
@@ -0,0 +1,15 @@
+import pytest
+import polyunite
+
+
+@pytest.mark.parametrize(
+    'is_heuristic, engine, family', [
+        (True, 'Alibaba', 'Linux/Agent.981bab81'),
+        (True, 'alibaba', 'Linux/Agent.981bab81'),
+        (False, 'NOT A REAL ENGINE', 'Linux/Agent.981bab81'),
+        (False, 'alibaba', None),
+        (False, 'alibaba', 1923),
+    ]
+)
+def test_is_heuristic(is_heuristic, engine, family):
+    assert is_heuristic == polyunite.is_heuristic(engine, family)
