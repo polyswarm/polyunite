e680752ea92f4692ef2774badbda5ffad6577c5f
Zephyr Pellerin <zephyr.pellerin@gmail.com>
2021-02-12 01:23:13 -0800

    Add "quick test" module

--- tests/test_parsers.py
+++ tests/test_parsers.py
@@ -6,41 +6,18 @@ import polyunite
 from .conftest import open_fixture
 
 
-
-# def test_parsers():
-#     """
-#     Speedier tests, stops at first error.
-#     """
-#     for report_results in json.loads(open_fixture('report_results.json')):
-#         engine = report_results['engine']
-#         source = report_results['source']
-#         actual = polyunite.parse(engine, source)
-#         if 'name' in report_results:
-#             expected_name = report_results['name']
-#             if expected_name != actual.name:
-#                 pytest.fail(
-#                     f'[{engine}/{source}] name={actual.name} expected={expected_name}',
-#                     pytrace=False,
-#                 )
-#         if 'labels' in report_results:
-#             expected_labels = set(report_results['labels'])
-#             if expected_labels != actual.labels:
-#                 pytest.fail(
-#                     f'[{engine}/{source}] labels={actual.labels}, expected={expected_labels}',
-#                     pytrace=False,
-#                 )
-
-
 @pytest.mark.parametrize(
-    'report_results',
-    json.loads(open_fixture('report_results.json')),
-    ids='{engine}/{source}'.format_map,
+    'engine,source,name,labels', [
+        pytest.param(
+            d['engine'],
+            d['source'],
+            d['name'],
+            d.get('labels', []),
+            id='{engine}/{source}'.format_map(d),
+        ) for d in json.loads(open_fixture('report_results.json'))
+    ]
 )
-def test_parsers(report_results):
-    actual = polyunite.parse(report_results['engine'], report_results['source'])
-
-    if 'name' in report_results:
-        assert report_results['name'] == actual.name
-
-    if 'labels' in report_results:
-        assert set(report_results['labels']) == actual.labels
+def test_parsers(engine, source, name, labels):
+    actual = polyunite.parse(engine, source)
+    assert name == actual.name
+    assert set(labels) == actual.labels
