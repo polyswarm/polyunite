8584db478c5f92701fe860f57d8bba484ca2b57a
zv <zephyr.pellerin@gmail.com>
2021-04-27 13:28:25 -0700

    Add `jsc` to Javascript `LANGS` pattern

--- polyunite/parsers/QuickHeal.py
+++ polyunite/parsers/QuickHeal.py
@@ -6,7 +6,7 @@ class QuickHeal(Classification):
     pattern = rf"""^
     (
         ([./]|^)
-        ({PLATFORM}|{LABELS}(?&LABELS)?|Cmd|PIF|alware|JSc)
+        ({PLATFORM}|{LABELS}(?&LABELS)?|Cmd|PIF|alware)
     )*
     (?P<VEID>
         (
