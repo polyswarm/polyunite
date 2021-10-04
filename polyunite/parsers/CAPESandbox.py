c996401643504db4287588374296f135348483eb
zv <zephyr.pellerin@gmail.com>
2021-10-04 12:26:35 -0700

    Run yapf/isort

--- polyunite/parsers/CAPESandbox.py
+++ polyunite/parsers/CAPESandbox.py
@@ -1,5 +1,10 @@
-from ..vocab import FAMILY_ID, TYPES, VARIANT_ID
+from ..vocab import FAMILY_ID
 from ._base import Classification
 
+
 class CapeSandbox(Classification):
-    pattern = rf"""^(?:family[:./-]?)?(?P<VEID>{FAMILY_ID('(?&TYPES)', r'[[:alnum:]]+')})$"""
+    pattern = rf"""^
+    (?:family[:./-]?)?
+    (?P<VEID>
+        {FAMILY_ID('(?&TYPES)', r'[[:alnum:]]+')}
+    )$"""
