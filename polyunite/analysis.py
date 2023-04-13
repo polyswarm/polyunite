69acfc90dbda316877814b3029b5ae47c944f7d5
Alan Justino <alan.justino@yahoo.com.br>
2023-04-13 16:53:48 -0300

    Classifier.family can now return str or list
    And everything will keep working as before

--- polyunite/analysis.py
+++ polyunite/analysis.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from typing import TYPE_CHECKING, Callable, Iterable, Optional, Tuple, Union
 
 from collections import Counter, UserDict
@@ -97,31 +98,34 @@ class Analyses(UserDict):
         for engine, clf in self.items():
             weight = weights.get(engine, 1.0)
 
-            name = clf.family
+            names: str | list[str] = clf.family
+            if isinstance(names, str):
+                names = [names]
 
-            if name is None:
-                name = clf.taxon
-                weight *= taxon_weight
+            for name in names:
+                if name is None:
+                    name = clf.taxon
+                    weight *= taxon_weight
 
-            if not isinstance(name, str):
-                continue
+                if not isinstance(name, str):
+                    continue
 
-            if len(name) < 2:
-                weight = 0.0
-            elif len(name) < 5:
-                weight *= len(name) / 5
-            elif len(name) > 10:
-                # Lower the weight of names longer than 10 chars
-                weight /= math.log(len(name), 10)
+                if len(name) < 2:
+                    weight = 0.0
+                elif len(name) < 5:
+                    weight *= len(name) / 5
+                elif len(name) > 10:
+                    # Lower the weight of names longer than 10 chars
+                    weight /= math.log(len(name), 10)
 
-            # Match `name` against the pattern predicates in `name_weights`, adjusting appropriately
-            if weight > 0:
-                for predicate, adjustment in name_weights.items():
-                    if predicate(name):
-                        weight *= adjustment
-                        break
+                # Match `name` against the pattern predicates in `name_weights`, adjusting appropriately
+                if weight > 0:
+                    for predicate, adjustment in name_weights.items():
+                        if predicate(name):
+                            weight *= adjustment
+                            break
 
-            yield name, weight
+                yield name, weight
 
     def _weighted_name_inference(self, names: Iterable[Tuple[str, float]]) -> Optional[str]:
         likelihood = self._weighted_name_likelihood(names)
