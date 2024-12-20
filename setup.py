3189f5395020cf840d6816a09b4f9114d5157f07
Samuel Barbosa <samuel.neto@polyswarm.io>
2024-12-20 17:25:29 -0300

    upgrading rapidfuzz

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     description="polyunite parses a variety of antimalware vendor's classification strings",
     install_requires=[
         'regex>=2020.11.13',
-        'rapidfuzz~=1.0',
+        'rapidfuzz>=1.0',
     ],
     long_description='file: README.rst',
     long_description_content_type='rst',
